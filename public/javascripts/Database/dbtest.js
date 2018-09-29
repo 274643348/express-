4bd8c67964e20f1f88f3c460b29359140785b4c8
274643348 <274643348@qq.com>
2018-09-29 16:55:23 +0800

    完成数据库的增删改查操作

--- public/javascripts/Database/dbtest.js
+++ public/javascripts/Database/dbtest.js
@@ -1,9 +1,50 @@
 var db = require('./mysql.js');
-var sql = 'SELECT count(*) as count from user';
-db.query(sql, function(err, rows, fields) {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  console.log('用户数量 : ', rows[0].count);
-});
+
+
+//////////////////////////////////////////查询
+// var sql = 'SELECT *  from aa';
+// db.query(sql, function (err, rows, fields) {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+
+//   console.log(rows);
+//   //RowDataPacket对象转为json字符串；
+//   let rowsStr = JSON.stringify(rows);
+//   console.log(rowsStr);
+//   let rowJson = JSON.parse(rowsStr);
+//   console.log(rowJson);
+//   console.log('用户数量 : ' + rowJson[0].userid);
+// });
+
+
+//////////////////////////////////////////插入
+// var sql = 'insert into aa(Count,sex) value (33,0)';
+// db.query(sql, function (err, rows, fields) {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+//   console.log(rows);
+// });
+
+//////////////////////////////////////////删除u
+// var sql = 'delete from aa where count = 33';
+// db.query(sql, function (err, rows, fields) {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+//   console.log(rows);
+// });
+
+//////////////////////////////////////////更新u
+// var sql = 'update  aa set count = 888, sex = 1 where userid<10';
+// db.query(sql, function (err, rows, fields) {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+//   console.log(rows);
+// });
--- public/javascripts/Database/mysql.js
+++ public/javascripts/Database/mysql.js
@@ -1,49 +1,29 @@
-// var db    = {};
-// var mysql = require('mysql');
-// var pool  = mysql.createPool({
-//   connectionLimit : 10,
-//   host            : 'localhost',
-//   user            : 'root',
-//   password        : '123456',
-//   database        : 'nodejs'
-// });
-
-// db.query = function(sql, callback){
-
-// 	if (!sql) {
-// 		callback();
-// 		return;
-// 	}
-// 	pool.query(sql, function(err, rows, fields) {
-// 	  if (err) {
-// 	    console.log(err);
-// 	    callback(err, null);
-// 	    return;
-// 	  };
-
-// 	  callback(null, rows, fields);
-// 	});
-// }
-// module.exports = db;
-
+var db = {};
 var mysql = require('mysql');
-var connection = mysql.createConnection({
+var pool = mysql.createPool({
+  connectionLimit: 10,
   host: 'localhost',
   user: 'root',
   password: 'ljy19931216',
-  port: '3306',
-  database: 'test1',
+  database: 'test1'
 });
 
-connection.connect();
+db.query = function (sql, callback) {
 
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) {
-    console.log(err);
+  if (!sql) {
+    callback();
     return;
   }
+  pool.query(sql, function (err, rows, fields) {
+    if (err) {
+      console.log(err);
+      callback(err, null);
+      return;
+    };
 
-  console.log('The solution is: ', rows[0].solution);
-});
+    callback(null, rows, fields);
+  });
+}
+module.exports = db;
 
-connection.end();
+
